27d8fa5dd93eb9809605f6646ee76843d1c6d8fc
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-02-13 20:04:39 +0000

    Updated Dockerfile to use apk add to install packages, and removed install_python.sh

--- lambda_python_package/src/main.sh
+++ lambda_python_package/src/main.sh
@@ -3,12 +3,9 @@
 function main {
   # Source the other files to gain access to their functions
   scriptDir=$(dirname ${0})
-  source ${scriptDir}/install_python.sh
   source ${scriptDir}/install_dependencies.sh
   source ${scriptDir}/test_python.sh
   source ${scriptDir}/zip_package.sh
-
-  install_python
 
   install_dependencies "$2"
 
